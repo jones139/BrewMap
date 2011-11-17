e42b4751fa68ceeb2e57bee5b7035b191727d5fb
Graham Jones <grahamjones139@gmail.com>
2011-11-17 21:43:40 +0000

    Added cider_house and perry_house to statistics, just in case they are mapped that way.

--- server/stats.sql
+++ server/stats.sql
@@ -2,7 +2,7 @@ select count(osm_id) as dist_point from planet_osm_point where craft='distillery
 select count(osm_id) as dist_poly from planet_osm_polygon where craft='distillery' or industry='distillery';
 select count(osm_id) as wine_point from planet_osm_point where craft='winery' or industry='winery';
 select count(osm_id) as wine_poly from planet_osm_polygon where craft='winery' or industry='winery';
-select count(osm_id) as cider_point from planet_osm_point where craft='cider' or industry='cider';
-select count(osm_id) as cider_poly from planet_osm_polygon where craft='cider' or industry='cider';
-select count(osm_id) as perry_point from planet_osm_point where craft='perry' or industry='perry';
-select count(osm_id) as perry_poly from planet_osm_polygon where craft='perry' or industry='perry';
+select count(osm_id) as cider_point from planet_osm_point where craft='cider' or industry='cider' or craft='cider_house' or industry='cider_house';
+select count(osm_id) as cider_poly from planet_osm_polygon where craft='cider' or industry='cider'or craft='cider_house' or industry='cider_house';
+select count(osm_id) as perry_point from planet_osm_point where craft='perry' or industry='perry'or craft='perry_house' or industry='perry_house';
+select count(osm_id) as perry_poly from planet_osm_polygon where craft='perry' or industry='perry'or craft='perry_house' or industry='perry_house';
