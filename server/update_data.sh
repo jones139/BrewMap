2a644fb5675ddcd92f56d5676cb9fc1c0429e6c8
Graham Jones <graham@marx.(none)>
2011-11-19 23:00:17 +0000

    Added distilleries and cider/perry producers (still no tagQueries for these though).

--- server/update_data.sh
+++ server/update_data.sh
@@ -3,5 +3,5 @@ SCRIPTDIR="$( cd -P "$( dirname "$0" )" && pwd )"
 cd $SCRIPTDIR
 $SCRIPTDIR/make_brew_json.py
 date >ncftp.log
-ncftpput -m -f ~/.ncftp_data -R /public_html/BrewMap/server *.json >>ncftp.log 2>&1
+ncftpput -m -f ~/.ncftp_data -R /public_html/BrewMap/server *.json BrewMap.cfg >>ncftp.log 2>&1
 
