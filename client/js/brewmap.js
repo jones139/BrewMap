4167b417823c63b2bc5a6dc72344747f59129b43
Graham Jones <grahamjones139@gmail.com>
2011-11-19 23:09:38 +0000

    Reduced icon size (still hard coded in brewmap.js...)

--- client/js/brewmap.js
+++ client/js/brewmap.js
@@ -35,10 +35,10 @@ function makeIcons() {
 	var iconType = L.Icon.extend({
 	    iconUrl: iconURL,
 	    shadowUrl: iconURL,
-	    iconSize: new L.Point(24,24),
-	    shadowSize: new L.Point(24,24),
-	    iconAnchor: new L.Point(12,12),
-	    popupAnchor: new L.Point(12,24)
+	    iconSize: new L.Point(16,16),
+	    shadowSize: new L.Point(16,16),
+	    iconAnchor: new L.Point(8,8),
+	    popupAnchor: new L.Point(8,24)
 	});
 	layers[layerName]['icon'] = new iconType();
     }
